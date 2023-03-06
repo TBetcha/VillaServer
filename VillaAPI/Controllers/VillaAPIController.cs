7583b3d326101de35722d7c40d74739a23f2542f
TMB <troy.boettger@gmail.com>
2023-03-05 22:20:15 -0500

    add patch call and xml support since everyone uses that these days

--- VillaAPI/Controllers/VillaAPIController.cs
+++ VillaAPI/Controllers/VillaAPIController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using VillaAPI.Data;
 using VillaAPI.Models.DTO;
@@ -86,6 +87,22 @@ public class VillaAPIController : ControllerBase
         villa.Name = villaDTO.Name;
         villa.SqFt = villaDTO.SqFt;
         villa.Name = villaDTO.Name;
+        return NoContent();
+    }
+
+    [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+    {
+        if (patchDTO == null || id == 0) return BadRequest();
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        if (villa == null) return BadRequest();
+
+        patchDTO.ApplyTo(villa, ModelState);
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
         return NoContent();
     }
 }
